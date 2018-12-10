780b8ca859a5ffc05d1a5c37bbb3844158843c5c
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-10 18:57:33 +0600

    use rxjs subject which is an alternative of ng event emitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './app-routing.module';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, Observable, Observer } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Observable, Observer, Subscription } from 'rxjs';
 
 
 @Component({
@@ -7,17 +7,19 @@ import { interval, Observable, Observer } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  numberObsSubscription: Subscription;
+  myObservableSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
-    /* const myNumbers = interval(3000);
-    myNumbers.subscribe(
+    const myNumbers = interval(3000);
+    this.numberObsSubscription = myNumbers.subscribe(
       (myNumber: number) => {
         console.log(myNumber);
       }
-    ); */
+    );
 
     const myObservable = Observable.create(
       (observer: Observer<string>) => {
@@ -39,12 +41,17 @@ export class HomeComponent implements OnInit {
       }
     );
 
-    myObservable.subscribe(
+    this.myObservableSubscription = myObservable.subscribe(
       (data: string) => {  console.log(data); },
       (error: string) => {  console.log(error); },
       () => {  console.log('completed.'); },
     );
 
+  }
+
+  ngOnDestroy() {
+    this.numberObsSubscription.unsubscribe();
+    this.myObservableSubscription.unsubscribe();
   }
 
 }
